82d50d9a42bcf2aef86cbe314727555050c9c5d2
Stefan Horning <stefan@hornings.de>
2020-06-26 17:44:02 +0200

    Fixed serverspec expectation, more tests. Fixed install path var. And added restart handling

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -15,13 +15,19 @@ describe "Metabase setup" do
 
   describe file('/etc/default/metabase') do
     it { should be_file }
-    its(:content) { should include('MB_PASSWORD_COMPLEXITY=10') }
+    its(:content) { should include('MB_PASSWORD_COMPLEXITY=strong') }
     its(:content) { should include('MB_JETTY_PORT=3000') }
+    its(:content) { should include('MB_DB_FILE=/opt/metabase/metabase.db') }
   end
 
   describe file('/etc/systemd/system/metabase.service') do
     it { should be_file }
     its(:content) { should include('EnvironmentFile=/etc/default/metabase') }
     its(:content) { should include('ExecStart=/usr/bin/java -jar /opt/metabase/metabase.jar') }
+  end
+
+  describe service('metabase') do
+    it { should be_enabled }
+    it { should be_running }
   end
 end
